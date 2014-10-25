1c8d3280bda339cd8021f65bc6fbb9b70cff52e2
Marcin Bugala <boogen@gmail.com>
2014-10-25 11:57:47 +0200

    little changes

--- Assets/scripts/CameraFollow.cs
+++ Assets/scripts/CameraFollow.cs
@@ -12,7 +12,7 @@ public class CameraFollow : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		float new_x = Mathf.Max(-5f, Mathf.Min(13f,  Mathf.Lerp(Camera.main.transform.position.x, this.transform.position.x, 4 * Time.deltaTime)));
+		float new_x = Mathf.Max(-5f, Mathf.Min(13f,  Mathf.Lerp(Camera.main.transform.position.x, this.transform.position.x - 2, 4 * Time.deltaTime)));
 		float new_y = Mathf.Max(0.5f, Mathf.Min(3f, Mathf.Lerp(Camera.main.transform.position.y, this.transform.position.y + 1.3f, 4 * Time.deltaTime)));
 		Camera.main.transform.position = new Vector3(new_x, new_y, offset.z);
 	}
--- Assets/scripts/Game.cs
+++ Assets/scripts/Game.cs
@@ -3,13 +3,17 @@ using System.Collections;
 
 public class Game : MonoBehaviour {
 
+	bool initialized = false;
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+		if (!initialized && Input.GetMouseButtonDown(0)) {
+			initialized = true;
+			GameObject.Find("micro_bat_1").AddComponent<CameraFollow>();
+		}
 	}
 }
