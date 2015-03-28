cd8170c894e27177974aa9440d2e179fad0e6acc
Marcin Bugala <marcin.bugala@tensquaregames.com>
2015-03-28 14:53:40 +0100

    Zajecia 2

---
+++ Assets/NewBehaviourScript1.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+				Rigidbody rb = GetComponent<Rigidbody> ();
+				rb.AddForce(new Vector3(-300, 700, 0));
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/lol.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lol : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Rigidbody rb = GetComponent<Rigidbody> ();
+		rb.AddForce (new Vector3 ( 0, 0, 500));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
