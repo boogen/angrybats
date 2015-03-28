e8ae0a03d67f32e8d31e8f76d4e0a8b7f411d78a
Marcin Bugala <marcin.bugala@tensquaregames.com>
2015-03-28 14:39:37 +0100

    zajecia 2

---
+++ Assets/shfhuufff.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shfhuufff : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Rigidbody ghd = GetComponent<Rigidbody>();
+		ghd.AddForce(new Vector3(-200, 500, 0));
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
