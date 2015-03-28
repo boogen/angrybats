651cd96bc5ac43a4a929bbb32005be4cb1b4f3d5
Marcin Bugala <marcin.bugala@tensquaregames.com>
2015-03-28 14:42:54 +0100

    zajecia 2

---
+++ Assets/Latanie_f_lewo.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Latanie_f_lewo : MonoBehaviour {
+
+	void Start () {
+		Rigidbody ptak = GetComponent<Rigidbody> ();
+		ptak.AddForce(new Vector3 (50, 0, 0));
+	}
+
+	void Update () {}
+}
+
